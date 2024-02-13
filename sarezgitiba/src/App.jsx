35779ba154e2ace749e055927e898a55bab6fed3
bezgaliba <mpsamarels@gmail.com>
2024-02-13 15:58:47 +0200

    set up split screen layout

--- sarezgitiba/src/App.jsx
+++ sarezgitiba/src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          sarezgitiba to be done. new deployment 12/02
-        </p>
-      </header>
+  <div class="parent">
+  
+  <div class="item" style={{background: 'url(https://unsplash.it/800/300) center center no-repeat', backgroundSize: 'cover'}}></div>
+    
+    <div class="item last">
+      <h2>Flexbox - Split Screen Layout</h2>
+      <p>Example of a split screen layout that uses flexbox.</p>
+      <p>A media query is used to detect if the virewport is below 600px, if it is then <b>flex-direction</b> is set to column-reverse, so that the image is displayed after the copy, useful on mobile devices!.</p>
+      <p>Reduce the window size to below 600px to see an example.</p>
     </div>
+    
+  </div>
   );
 }
 
